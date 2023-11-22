aa219a0021c8f90627162b5ce67dcc1e722300b3
Pavel Gavrilov <gavrilovpasha27@gmail.com>
2023-11-22 20:25:40 +0300

    first commit

---
+++ Main.swift
@@ -0,0 +1,18 @@
+//
+//  Main.swift
+//  CashFlow
+//
+//  Created by Паша on 22.11.23.
+//
+
+import SwiftUI
+
+struct Main: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Main()
+}
