d37d9d5d839b93bf3aabe64773a6d5199b08d7de
Pavel Gavrilov <gavrilovpasha27@gmail.com>
2023-11-22 15:59:32 +0300

    Initial Commit

---
+++ CashFlow/CashFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CashFlowApp.swift
+//  CashFlow
+//
+//  Created by Паша on 22.11.23.
+//
+
+import SwiftUI
+
+@main
+struct CashFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
