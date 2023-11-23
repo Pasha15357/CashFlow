30b44a2925959fbc5434443922e3efa1672fdf2d
Pavel Gavrilov <gavrilovpasha27@gmail.com>
2023-11-23 22:05:42 +0300

    adding category, adding expense, settings with profile,main page

---
+++ CashFlow/AddCategory.swift
@@ -0,0 +1,18 @@
+//
+//  AddCategory.swift
+//  CashFlow
+//
+//  Created by Паша on 23.11.23.
+//
+
+import SwiftUI
+
+struct AddCategory: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddCategory()
+}
---
+++ CashFlow/Settings.swift
@@ -0,0 +1,18 @@
+//
+//  Settings.swift
+//  CashFlow
+//
+//  Created by Паша on 23.11.23.
+//
+
+import SwiftUI
+
+struct Settings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Settings()
+}
