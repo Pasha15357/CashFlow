62c580b543ac315a3ed7f9f3f80c3f4dad571364
Pavel Gavrilov <gavrilovpasha27@gmail.com>
2024-02-29 11:37:22 +0300

    working with Categories (Core Data)

---
+++ CashFlow/Views/Category/EditCategoryView.swift
@@ -0,0 +1,18 @@
+//
+//  EditCategoryView.swift
+//  CashFlow
+//
+//  Created by Паша on 29.02.24.
+//
+
+import SwiftUI
+
+struct EditCategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditCategoryView()
+}
